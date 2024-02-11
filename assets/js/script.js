fed9855d95e47990d9dc58610117e45f6f44926c
niall-code <niallcaplis@gmail.com>
2024-02-11 16:39:57 +0000

    add code to check answer and respond to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
   let buttons = document.getElementsByTagName('button');
   for (let button of buttons) {
     button.addEventListener('click', function() {
-      // Feedback when user submits answer
+      
+      // When user submits answer, check it
       if (this.getAttribute('data-type') === 'submit') {
-        alert('You clicked Submit');
+        checkAnswer();
       } else {
+
         // Allow user to select game mode
         let gameType = this.getAttribute('data-type');
         runGame(gameType);
@@ -20,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
- */
+*/
 function runGame(gameType) {
   // Random numbers, from 1 to 25
   let num1 = Math.floor(Math.random() * 25) + 1;
@@ -29,6 +31,7 @@ function runGame(gameType) {
   if (gameType === 'addition') {
     displayAdditionQuestion(num1, num2);
   } else {
+    
     // Error message
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Game stopped`
@@ -59,12 +62,45 @@ function displayDivisionQuestion(operand1, operand2) {
   document.getElementById('operator').textContent = '÷';
 }
 
+/**
+ * Gets the operands and operator (numbers and symbol)
+ * from the DOM, and returns the correct answer.
+*/
 function calculateCorrectAnswer() {
+  let operand1 = parseInt(document.getElementById('operand1').innerText);
+  let operand2 = parseInt(document.getElementById('operand2').innerText);
+  let operator = document.getElementById('operator').innerText;
 
+  if (operator === '+') {
+    // Returns answer and "game mode to continue" in an array
+    return [operand1 + operand2, 'addition'];
+  } else {
+
+    // Error message
+    alert(`Unimplemented operator ${operator}`);
+    throw `Unimplemented operator ${operator}. Game stopped`;
+  }
 }
 
+/**
+ * Checks the user's answer against the first element in
+ * the returned calculateCorrectAnswer array
+*/
 function checkAnswer() {
+  let userAnswer = parseInt(document.getElementById('answer-box').value);
+  let calculatedAnswer = calculateCorrectAnswer();
+  let isCorrect = userAnswer === calculatedAnswer[0];
 
+  // Feedback whether correct or incorrect
+  if (isCorrect) {
+    alert("Well done. That's the right answer.");
+  } else {
+    alert(`Sorry, ${userAnswer} wasn't the right answer.
+    The answer was ${calculatedAnswer[0]}.`);
+  }
+
+  // New game
+  runGame(calculatedAnswer[1]);
 }
 
 function incrementScore() {
