45943d52edcdc128b4dcf3b68f0e51652a5ee58a
niall-code <niallcaplis@gmail.com>
2024-02-11 14:13:38 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+  
+}
+
+function displayMultiplyQuestion() {
+  
+}
+
+function displayDivisionQuestion() {
+  
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
